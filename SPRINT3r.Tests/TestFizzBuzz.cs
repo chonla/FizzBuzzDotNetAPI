b056a84f6a5bf6dc2d5f9a6880c8e5d0be50c693
Chonlasith Jucksriporn <chonla@capabilify.com>
2016-12-25 11:13:10 +0700

    Initial with count 1

---
+++ SPRINT3r.Tests/TestFizzBuzz.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using SPRINT3r;
+
+namespace TestSPRINT3r
+{
+	[TestFixture()]
+	public class TestFizzBuzz
+	{
+		[Test()]
+		public void ItShouldCount1()
+		{
+			var fizzBuzz = new FizzBuzz();
+			Assert.AreEqual("1", fizzBuzz.count(1));
+		}
+	}
+}
