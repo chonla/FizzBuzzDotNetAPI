fd8b189b19f82104bc0d893baf7b91e6031cc442
Chonlasith Jucksriporn <chonla@capabilify.com>
2016-12-25 23:37:59 +0700

    add fizzbuzz

---
+++ SPRINT3r/FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace SPRINT3r
+{
+	public class FizzBuzz
+	{
+		public FizzBuzz()
+		{
+		}
+
+		public string Count(int v)
+		{
+			if (isBuzz(v) && IsFizz(v))
+			{
+				return "FizzBuzz";
+			}
+			if (IsFizz(v))
+			{
+				return "Fizz";
+			}
+			if (isBuzz(v))
+			{
+				return "Buzz";
+			}
+			return v.ToString();
+		}
+
+		static bool IsFizz(int v)
+		{
+			return v % 3 == 0;
+		}
+
+		static bool isBuzz(int v)
+		{
+			return v % 5 == 0;
+		}
+	}
+}
